6579259c356caf1f7dca8b9f64bf82e12eb8a540
roypa123 <roypa81130@gmail.com>
2026-09-11 18:08:47 +0530

    fix: some changes

---
+++ ecommerce-backend/app/core/security.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt 
+from passlib.context import CryptContext
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM ="HS256"
+ACCESS_TOKEN_EXPIRES_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+
+def hash_password(password:str) -> str:
+    return pwd_context.has(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def _create_token(data: dict, expires_delta: timedelta, token_type: str) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp":expire, "type": token_type})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def create_access_token(data: dict) -> str:
+    return _create_token(data,timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES), "access")
+
+def create_refresh_token(data: dict) -> str:
+    return _create_token(data, timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS), "refresh")
