5675fd99cbec5a00a834bd312e9c4a7fcf79a36d
roypa123 <roypa81130@gmail.com>
2026-09-11 22:37:28 +0530

    fix: some changes

--- ecommerce-backend/app/core/storage.py
+++ ecommerce-backend/app/core/storage.py
@@ -1,20 +1,28 @@
 import os
 import uuid
 
-
+import boto3
+from dotenv import load_dotenv
 from fastapi import UploadFile
 
-UPLOAD_DIR = "static/uploads"
+load_dotenv()
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+MINIO_ACCESS_KEY= os.getenv("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+MINIO_BUCKET = os.getenv("MINIO_BUCKET")
+MINIO_PUBLIC_URL = os.getenv("MINIO_PUBLIC_URL")
+
+s3_client = boto3.client(
+    "s3",
+    endpoint_url=MINIO_ENDPOINT,
+    aws_access_key_id=MINIO_ACCESS_KEY,
+    aws_secret_access_key=MINIO_ACCESS_KEY,
+    config=boto3.session.Config(s3={"addressing_style": "path"}),
+)
+
+
 
 def upload_image(file: UploadFile, folder: str) -> str:
     extension = file.filename.rsplit(".",1)[-1] if "." in file.filename else "bin"
-    filename = f"{uuid.uuid4()}.extension"
-
-    folder_path = os.path.join(UPLOAD_DIR, folder)
-    os.makedirs(folder_path, exist_ok=True)
-
-    file_path = os.path.join(folder_path, filename)
-    with open(file_path, "wb") as f:
-        f.write(file.file.read())
-
-    return f"/static/uploads/{folder}/{filename}"
+    
