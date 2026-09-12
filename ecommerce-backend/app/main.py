8bf2e217bca93a0aa3e847c7416b55d452e3ed99
roypa123 <roypa81130@gmail.com>
2026-09-12 14:42:34 +0530

    fiX: some changes

--- ecommerce-backend/app/main.py
+++ ecommerce-backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from app.routers.user import router as user_router
@@ -6,6 +7,14 @@ from app.routers.category import router as category_router
 from app.routers.product import router as product_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
